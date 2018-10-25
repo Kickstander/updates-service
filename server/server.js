86f41a1aadf57798bd1984677d329c2fdf36dcfa
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-25 00:08:29 -0700

    Add morgan and body-parser to server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
 const app = express();
 
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 app.listen(port);
