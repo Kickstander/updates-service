4795de8cd13e0a47d5a27dc0d1ad0f99c1edfcc6
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-27 20:22:21 -0700

    Cause app to dislay title from Dummy data on page load

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,17 @@
 const React = require('react');
 const ReactDom = require('react-dom');
+const dummyData = require('./dummyData.json');
 
-function App() {
-  return <div>Hello React</div>;
+function App(props) {
+  return (
+    <div>
+      <div>REACT SAYS HI!</div>
+      <div>{props.data[0].title}</div>
+    </div>
+  );
 }
 
-document.addEventListener('DOMContentLoaded', e => {
-  ReactDom.render(<App />, document.getElementById('root'));
+document.addEventListener('DOMContentLoaded', () => {
+  console.log(dummyData);
+  ReactDom.render(<App data={dummyData} />, document.getElementById('root'));
 });
