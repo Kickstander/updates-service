6fb8e3aecba1b1ddd88e38b1ec3290901008ff3a
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-29 17:50:00 -0700

    Add horizontal line for a timeline entry

--- client/components/preview.jsx
+++ client/components/preview.jsx
@@ -6,26 +6,32 @@ function Preview(props) {
   if (props.side === 'left') {
     dateHeader = (
       <div className="preview-header">
-        <div>Date</div>
-        <div className="spacer-container">spacers</div>
+        <div className="date">Date</div>
+        <div className="spacer-container">
+          <div className="spacer-line" />
+        </div>
       </div>
     );
   } else {
     dateHeader = (
       <div className="preview-header">
-        <div className="spacer-container">spacers</div>
-        <div>Date</div>
+        <div className="spacer-container">
+          <div className="spacer-line" />
+        </div>
+        <div className="date">Date</div>
       </div>
-    )
+    );
   }
 
   return (
     <div className={`preview-container ${props.side}`}>
       <div className={`preview preview-${props.side}`}>
         {dateHeader}
-        <div className="preview-title">Title of Update</div>
-        <div className="preview-body">Some body text here</div>
-        <div className="preview-footer">Comments Likes</div>
+        <div className="preview-main">
+          <div className="preview-title">Title of Update</div>
+          <div className="preview-body">Some body text here</div>
+          <div className="preview-footer">Comments Likes</div>
+        </div>
       </div>
     </div>
   );
