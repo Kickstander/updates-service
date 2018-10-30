aab91ed27f5740f205758694b9d2ddca141101f2
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-29 17:20:13 -0700

    add padding to preview and fix spacers bug

--- client/components/preview.jsx
+++ client/components/preview.jsx
@@ -1,13 +1,28 @@
 const React = require('react');
 
 function Preview(props) {
+  let dateHeader;
+
+  if (props.side === 'left') {
+    dateHeader = (
+      <div className="preview-header">
+        <div>Date</div>
+        <div className="spacer-container">spacers</div>
+      </div>
+    );
+  } else {
+    dateHeader = (
+      <div className="preview-header">
+        <div className="spacer-container">spacers</div>
+        <div>Date</div>
+      </div>
+    )
+  }
+
   return (
     <div className={`preview-container ${props.side}`}>
       <div className={`preview preview-${props.side}`}>
-        <div className="preview-header">
-          <div className="spacer-container">spacers</div>
-          <div>Date</div>
-        </div>
+        {dateHeader}
         <div className="preview-title">Title of Update</div>
         <div className="preview-body">Some body text here</div>
         <div className="preview-footer">Comments Likes</div>
