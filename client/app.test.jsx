0646c599cabec2ad18339e541c9aa808e542bb07
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-31 19:39:13 -0700

    Add test to make sure that App component renders

--- client/app.test.jsx
+++ client/app.test.jsx
@@ -5,7 +5,12 @@ import dummyData from './dummyData.json';
 // import { shallow } from 'enzyme';
 
 describe('<App />', () => {
-  test('Renders a <Preview /> component', () => {
+  test('<App /> Renders', () => {
+    const wrapper = shallow(<App updates={[]} />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  test('Renders a list of <Preview /> components', () => {
     const wrapper = shallow(<App updates={dummyData} />);
     expect(wrapper.find(Preview).length).toBe(3);
   });
