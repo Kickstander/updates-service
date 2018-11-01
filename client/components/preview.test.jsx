3defb40bae31a7f72fb7c57ec421d6d9ef26f22b
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-31 19:47:09 -0700

    Add basic existence test for preview component

---
+++ client/components/preview.test.jsx
@@ -0,0 +1,13 @@
+import dummyData from '../dummyData.json';
+import Preview from './preview.jsx';
+
+const update = dummyData[0];
+
+describe('<Preview />', () => {
+  test('it exists', () => {
+    const wrapper = shallow(<Preview update={update} />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  test('')
+});
