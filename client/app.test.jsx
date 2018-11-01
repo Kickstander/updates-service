606efdba9a70598f306fe93136e8871a1c377178
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-31 19:06:15 -0700

    Add mock css files to avoid issues with CSS Modules and jest

--- client/app.test.jsx
+++ client/app.test.jsx
@@ -1,11 +1,12 @@
 /* eslint-evn jest */
-// import App from './app.jsx';
-// import Preview from './components/preview';
+import App from './app.jsx';
+import Preview from './components/preview.jsx';
+import dummyData from './dummyData.json';
 // import { shallow } from 'enzyme';
 
 describe('<App />', () => {
   test('Renders a <Preview /> component', () => {
-    const rDiv = shallow(<div className='sup' />);
-    expect(rDiv.hasClass('sup')).toBe(true);
+    const wrapper = shallow(<App updates={dummyData} />);
+    expect(wrapper.find(Preview).length).toBe(3);
   });
 });
