4cad8eec9f9e37424a4c1825c4af682c87079d74
Jordan Holmes <jordanstholmes@gmail.com>
2018-11-03 17:20:17 -0700

    Refactor app component to be a class component that does its own server request

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,57 +1,68 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
 import styles from './app.css';
 import Preview from './preview';
 
 let isLeft = true;
+const PROJECT_ID = 7;
 
 const alternateSide = () => {
   isLeft = !isLeft;
   return isLeft ? 'left' : 'right';
 };
 
-function App({ updates }) {
-  const updateComponents = updates.reduce((acc, update) => {
-    const side = alternateSide();
-    const preview = <Preview update={update} side={side} key={update.id} />;
-    const spacer = <div key={`${update.id}spacer`} />;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { updates: null };
+    this.createAlternatingUpdates = this.createAlternatingUpdates.bind(this);
+  }
 
-    if (side === 'left') {
-      acc.push(preview);
-      acc.push(spacer);
-    } else {
-      acc.push(spacer);
-      acc.push(preview);
-    }
-    return acc;
-  }, []);
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3004/${PROJECT_ID}/updates`)
+      .then(updates => {
+        this.setState({ updates: updates.data });
+      })
+      .catch(err => console.log(err));
+  }
 
-  return (
-    <div>
-      <div className={styles.verticalMargin} />
-      <div className={styles.wrapper}>
-        <div />
-        <div className={styles.contentWrapper}>{updateComponents}</div>
-        <div />
+  createAlternatingUpdates() {
+    const { updates } = this.state;
+    console.log(updates);
+    const updateComponents = updates.reduce((acc, update) => {
+      const side = alternateSide();
+      const preview = <Preview update={update} side={side} key={update.id} />;
+      const spacer = <div key={`${update.id}spacer`} />;
+
+      if (side === 'left') {
+        acc.push(preview);
+        acc.push(spacer);
+      } else {
+        acc.push(spacer);
+        acc.push(preview);
+      }
+      return acc;
+    }, []);
+    return updateComponents;
+  }
+
+  render() {
+    const { updates } = this.state;
+    return (
+      <div>
+        <div className={styles.verticalMargin} />
+        <div className={styles.wrapper}>
+          <div />
+          <div className={styles.contentWrapper}>
+            {updates ? this.createAlternatingUpdates() : <h1>Loading Updates...</h1>}
+          </div>
+          <div />
+        </div>
+        <div className={styles.verticalMargin} />
       </div>
-      <div className={styles.verticalMargin} />
-    </div>
-  );
+    );
+  }
 }
-
-App.defaultProps = {
-  updates: {}
-};
-
-App.propTypes = {
-  updates: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string,
-      pubDate: PropTypes.string,
-      likes: PropTypes.number,
-      body: PropTypes.string
-    })
-  )
-};
 
 export default App;
--- client/index.jsx
+++ client/index.jsx
@@ -1,13 +1,8 @@
 /* eslint-env browser */
 import React from 'react';
 import ReactDom from 'react-dom';
-import axios from 'axios';
 import App from './components/app';
 
-const PROJECT_ID = 5;
-
 document.addEventListener('DOMContentLoaded', () => {
-  axios.get(`http://localhost:3000/${PROJECT_ID}/updates`).then(response => {
-    ReactDom.render(<App updates={response.data} />, document.getElementById('root'));
-  });
+  ReactDom.render(<App />, document.getElementById('root'));
 });
