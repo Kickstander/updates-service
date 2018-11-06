1170206f8d502911d35a88025d5b5a7eed1fa33a
Jordan Holmes <jordanstholmes@gmail.com>
2018-11-05 16:52:07 -0800

    Switch to Rendering to the dom in bundled file

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,8 @@
 /* eslint-env browser */
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Updates from './components/app';
 
-window.Updates = Updates;
+// window.Updates = Updates;
+
+ReactDOM.render(<Updates />, document.getElementById('Updates'));
