4dd25f865ea90771893fcbfec91d141dcfe9d46b
Jordan Holmes <jordanstholmes@gmail.com>
2018-11-06 13:35:14 -0800

    Potentially fix foreign key issue during seeding

--- database/db.js
+++ database/db.js
@@ -1,11 +1,14 @@
-const DATABASE = 'kickstarter';
-const USERNAME = 'root';
 const Sequelize = require('sequelize');
+const mysql = require('mysql');
+const loginInfo = require('./db.env.config');
+const generateAllSeedData = require('./seedingUtils');
+
+const data = generateAllSeedData(100);
 
 const sequelize = new Sequelize({
-  database: DATABASE,
-  username: USERNAME,
-  password: null,
+  database: loginInfo.database,
+  username: loginInfo.user,
+  password: loginInfo.password,
   host: 'localhost',
   dialect: 'mysql',
   define: {
@@ -14,38 +17,61 @@ const sequelize = new Sequelize({
   sync: { force: true }
 });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('MYSQL connection has been established...');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
 const User = sequelize.define('user', {
   userName: Sequelize.STRING(100)
 });
-
 const Project = sequelize.define('project', {
   projectName: Sequelize.STRING
 });
-
+Project.belongsTo(User, { foreignKey: 'ownerId' });
 const Update = sequelize.define('update', {
   title: Sequelize.STRING,
   body: Sequelize.TEXT,
   likes: Sequelize.INTEGER,
   pubDate: Sequelize.DATE
 });
-
-Project.belongsTo(User, { foreignKey: 'ownerId' });
 Update.belongsTo(User, { foreignKey: 'postedBy' });
 Update.belongsTo(Project, { foreignKey: 'projectId' });
 
-User.sync();
-Project.sync();
-Update.sync();
+function seedDatabase() {
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log('MYSQL connection has been established...');
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err);
+    });
+  User.sync()
+    .then(() => {
+      return Project.sync();
+    })
+    .then(() => {
+      return Update.sync();
+    })
+    .then(() => User.bulkCreate(data.users))
+    .then(() => Project.bulkCreate(data.projects))
+    .then(() => Update.bulkCreate(data.updates))
+    .catch(err => console.error(err));
+}
 
-exports.User = User;
-exports.Project = Project;
-exports.Update = Update;
+const connection = mysql.createConnection(loginInfo);
+connection.connect();
+connection.query('DROP DATABASE if exists kickstarter;', dropErr => {
+  if (dropErr) {
+    return console.error(dropErr);
+  }
+  console.log('"kickstarter" database dropped');
+  connection.query('CREATE DATABASE kickstarter;', createErr => {
+    if (createErr) {
+      return console.error(createErr);
+    }
+    console.log('NEW "kickstarter" database created!');
+    connection.end();
+    seedDatabase();
+    return undefined;
+  });
+  return undefined;
+});
+
+module.exports = { User, Project, Update };
