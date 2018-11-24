0a17a428793bdbd6041bf320c3aca840426497a5
Joshua Elder <jcelder88@gmail.com>
2018-11-23 17:05:51 -0800

    Enable dotenv

--- server/database/db.js
+++ server/database/db.js
@@ -10,13 +10,14 @@ const {
   DB_HOST,
   DB_USER,
   DB_PASSWORD,
+  DATABASE,
   DB_PORT,
 } = process.env;
 
 const connOptions = {
   host: DB_HOST,
   port: DB_PORT,
-  database: 'kickstander',
+  database: DATABASE,
   user: DB_USER,
   password: DB_PASSWORD,
   max: 10,
